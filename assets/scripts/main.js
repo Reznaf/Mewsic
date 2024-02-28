91afef402d7816ed51133ceab28708eb3d8128a6
Reznaf <maldo.aufa@gmail.com>
2024-02-28 16:31:17 +0700

    Day 4
    Added music player basic function

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -6,35 +6,36 @@ musicAudio = wrapper.querySelector("#music-audio"),
 playPauseBtn = wrapper.querySelector(".play-pause"),
 prevBtn = wrapper.querySelector("#prev"),
 nextBtn = wrapper.querySelector("#next"),
-progressBar = wrapper.querySelector(".progress-bar");
+progressBar = wrapper.querySelector(".progress-bar"),
+progressArea = wrapper.querySelector(".progress-area");
 
-let musicIndex = 3;
+let musicIndex = 1;
 
-window.addEventListener("load" , ()=>{
+window.addEventListener("load", () => {
     loadMusic(musicIndex);
 })
 
-function loadMusic(indexNumb){
+function loadMusic(indexNumb) {
     musicName.innerText = allMusic[indexNumb - 1].name;
     musicArtist.innerText = allMusic[indexNumb - 1].artist;
     musicImg.src = './assets/images/' + allMusic[indexNumb - 1].img + ".png";
     musicAudio.src = './assets/music/' + allMusic[indexNumb - 1].src + ".mp3";
 }
 
-function playMusic(){
+function playMusic() {
     wrapper.classList.add("paused");
     playPauseBtn.querySelector("i").innerText = "pause";
     musicAudio.play();
 }
 
-function nextMusic(){
+function nextMusic() {
     musicIndex++;
     musicIndex > allMusic.length ? musicIndex = 1 : musicIndex = musicIndex;
     loadMusic(musicIndex)
     playMusic();
 }
 
-function prevMusic(){
+function prevMusic() {
     musicIndex--;
     musicIndex < 1 ? musicIndex = allMusic.length : musicIndex = musicIndex;
     loadMusic(musicIndex)
@@ -42,28 +43,98 @@ function prevMusic(){
 }
 
 
-function pauseMusic(){
+function pauseMusic() {
     wrapper.classList.remove("paused");
     playPauseBtn.querySelector("i").innerText = "play_arrow"
     musicAudio.pause();
 }
 
-playPauseBtn.addEventListener("click", ()=>{
+playPauseBtn.addEventListener("click", () => {
     const isMusicPaused = wrapper.classList.contains("paused");
     isMusicPaused ? pauseMusic() : playMusic();
 });
 
-nextBtn.addEventListener("click", ()=>{
+nextBtn.addEventListener("click", () => {
     nextMusic();
-})
+});
 
-prevBtn.addEventListener("click", ()=>{
+prevBtn.addEventListener("click", () => {
     prevMusic();
-})
+});
 
-musicAudio.addEventListener("timeupdate", (e)=>{
+musicAudio.addEventListener("timeupdate", (e) => {
     const currentTime = e.target.currentTime;
     const duration = e.target.duration;
     let progressWidth = (currentTime / duration) * 100;
-    progressBar.style.width = progressWidth;
+    progressBar.style.width = `${progressWidth}%`;
+
+    let musicCurrentTime = wrapper.querySelector(".current");
+    musicDuration = wrapper.querySelector(".duration");
+
+    musicAudio.addEventListener("loadeddata", () => {
+        let audioDuration = musicAudio.duration;
+        let totalMin = Math.floor(audioDuration / 60);
+        let totalSec = Math.floor(audioDuration % 60);
+        if (totalSec < 10) {
+            totalSec = `0${totalSec}`;
+        }
+        musicDuration.innerText = `${totalMin}:${totalSec}`;
+    });
+
+    let currentMin = Math.floor(currentTime / 60);
+    let currentSec = Math.floor(currentTime % 60);
+    if (currentSec < 10) {
+        currentSec = `0${currentSec}`;
+    }
+    musicCurrentTime.innerText = `${currentMin}:${currentSec}`;
 });
+
+progressArea.addEventListener("click", (e)=>{
+    let progressWidth = progressArea.clientWidth;
+    let clickedOffSetX = e.offsetX;
+    let songDuration = musicAudio.duration;
+    musicAudio.currentTime = (clickedOffSetX / progressWidth) * songDuration;
+    playMusic();
+
+});
+
+const repeatBtn = wrapper.querySelector("#repeat-plist");
+repeatBtn.addEventListener("click", ()=>{
+    let getText = repeatBtn.innerText;
+    switch(getText){
+        case "repeat":
+            repeatBtn.innerText = "repeat_one";
+            repeatBtn.setAttribute("title", "Song looped")
+            break;
+        case "repeat_one":
+            repeatBtn.innerText  = "shuffle";
+            repeatBtn.setAttribute("title", "Playback shuffle")
+            break;
+        case "shuffle":
+            repeatBtn.innerText  = "repeat";
+            repeatBtn.setAttribute("title", "Playlist looped")
+            break;
+    }
+})
+
+musicAudio.addEventListener("ended", ()=>{
+    let getText = repeatBtn.innerText;
+    switch(getText){
+        case "repeat":
+            nextMusic();
+            break;
+        case "repeat_one":
+            musicAudio.currentTime = 0;
+            playMusic();
+            break;
+        case "shuffle":
+            let randindex = Math.floor(math.random() * allMusic.length + 1);
+            do{
+                randindex = Math.floor(math.random() * allMusic.length + 1);
+            }while(musicIndex == randIndex);
+            musicIndex = randIndex;
+            loadMusic(musicIndex);
+            playMusic();
+            break;
+    }
+})
